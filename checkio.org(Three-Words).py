9074d4be01b773dd21262767cf1698794d943c9f
MichalHalucha <44180583+MichalHalucha@users.noreply.github.com>
2018-11-14 15:16:24 +0100

    Done

--- checkio.org(Three-Words).py
+++ checkio.org(Three-Words).py
@@ -1,18 +1,35 @@
 import re
-
 def checkio(words: str) -> bool:
-    if words == "Hi":
-        return False
-    if len(words)==47:
-        return True
-
-
-    for numbers in words.split():
-        if numbers.isdigit() or numbers.isnumeric():
-            return False
-
-    return True
-
+    words = words.lower()
+    list = words.split()
+    print(list)
+    count=0
+    i=0
+    if type(list[i]==str):
+        status="str"
+    else:
+        status="int"
+    for i in range(len(list)):
+        print(i)
+        print(type(list[i]))
+        if list[i].isnumeric():
+            if status != "int":
+                status = "int"
+                count = 0
+            if count==2:
+                print("Są 3 int pod rząd")
+                return False
+            count +=1
+        elif type(list[i]) == str:
+            if status != "str":
+                status = "str"
+                count = 0
+            if count==2:
+                print("Są 3 string pod rząd__________________")
+                return True
+            count +=1
+    print("Nie bylo nic pod rzad_____________________________")
+    return False
 
 # These "asserts" using only for self-checking and not necessary for auto-testing
 if __name__ == '__main__':
