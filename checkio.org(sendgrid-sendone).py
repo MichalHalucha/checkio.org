09a04c09e67b505acb4d8ae78fd0885233ad8f34
MichalHalucha <44180583+MichalHalucha@users.noreply.github.com>
2018-11-21 01:12:43 +0100

    Done

--- checkio.org(sendgrid-sendone).py
+++ checkio.org(sendgrid-sendone).py
@@ -21,3 +21,20 @@ if __name__ == '__main__':
     #These "asserts" using only for self-checking and not necessary for auto-testing
     send_email('somebody@gmail.com', 'Some Body')
     print('Done')
+    
+ """# using SendGrid's Python Library
+# https://github.com/sendgrid/sendgrid-python
+import sendgrid
+import os
+from sendgrid.helpers.mail import *
+
+sg = sendgrid.SendGridAPIClient(apikey=os.environ.get('SENDGRID_API_KEY'))
+from_email = Email("test@example.com")
+to_email = Email("test@example.com")
+subject = "Sending with SendGrid is Fun"
+content = Content("text/plain", "and easy to do anywhere, even with Python")
+mail = Mail(from_email, subject, to_email, content)
+response = sg.client.mail.send.post(request_body=mail.get())
+print(response.status_code)
+print(response.body)
+print(response.headers)"""
