da5e23798ff87b4cc7786722138f30f1118b7fdf
MichalHalucha <44180583+MichalHalucha@users.noreply.github.com>
2018-11-30 11:37:28 +0100

    Done

---
+++ checkio.org(binary-count).py
@@ -0,0 +1,19 @@
+def checkio(number):
+    x = bin(number)
+    result = 0
+    x = str(x)
+    print(x, type(x))
+    for char in x:
+        print(char)
+        if char == "1":
+            result +=1
+    print(result)
+    return result
+
+
+# These "asserts" using only for self-checking and not necessary for auto-testing
+if __name__ == '__main__':
+    assert checkio(4) == 1
+    assert checkio(15) == 4
+    assert checkio(1) == 1
+    assert checkio(1022) == 9
