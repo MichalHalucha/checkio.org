0e38094d47449082c6d8be2b77b360cb371e2fa0
MichalHalucha <44180583+MichalHalucha@users.noreply.github.com>
2018-12-10 13:18:05 +0100

    Done

---
+++ checkio.org(flatten-list).py
@@ -0,0 +1,16 @@
+def flat_list(a):
+    b = []
+    for x in a:
+        if type(x) is list:
+            b.extend(flat_list(x))
+        else:
+            b.append(x)
+            print(b)
+    return b
+
+if __name__ == '__main__':
+    assert flat_list([1, 2, 3]) == [1, 2, 3], "First"
+    assert flat_list([1, [2, 2, 2], 4]) == [1, 2, 2, 2, 4], "Second"
+    assert flat_list([[[2]], [4, [5, 6, [6], 6, 6, 6], 7]]) == [2, 4, 5, 6, 6, 6, 6, 6, 7], "Third"
+    assert flat_list([-1, [1, [-2], 1], -1]) == [-1, 1, -2, 1, -1], "Four"
+    print('Done! Check it')
