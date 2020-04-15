831572a06deb8140c2d3a3d95050fb27156781de
MichalHalucha <44180583+MichalHalucha@users.noreply.github.com>
2020-04-15 12:25:06 +0200

    Done

---
+++ checkio.org(remove_all).py
@@ -0,0 +1,27 @@
+from typing import Iterable
+
+
+def remove_all_before(items: list, border: int) -> Iterable:
+    try:
+        if items.index(border):
+            print(items)
+            print(border)
+            output = items[items.index(border):]
+            print(output)
+        else:
+            return items
+    except:
+        return items
+    return output
+
+
+if __name__ == '__main__':
+
+    # These "asserts" are used for self-checking and not for an auto-testing
+    assert list(remove_all_before([1, 2, 3, 4, 5], 3)) == [3, 4, 5]
+    assert list(remove_all_before([1, 1, 2, 2, 3, 3], 2)) == [2, 2, 3, 3]
+    assert list(remove_all_before([1, 1, 2, 4, 2, 3, 4], 2)) == [2, 4, 2, 3, 4]
+    assert list(remove_all_before([1, 1, 5, 6, 7], 2)) == [1, 1, 5, 6, 7]
+    assert list(remove_all_before([], 0)) == []
+    assert list(remove_all_before([7, 7, 7, 7, 7, 7, 7, 7, 7], 7)) == [7, 7, 7, 7, 7, 7, 7, 7, 7]
+    print("Coding complete? Click 'Check' to earn cool rewards!")
