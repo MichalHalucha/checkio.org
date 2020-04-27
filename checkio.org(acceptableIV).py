99e12a0963787b6cc4d26f1093f8c7fe548de069
MichalHalucha <44180583+MichalHalucha@users.noreply.github.com>
2020-04-27 17:07:22 +0200

    Done

---
+++ checkio.org(acceptableIV).py
@@ -0,0 +1,53 @@
+import re
+# Taken from mission Acceptable Password III
+
+def is_acceptable_password(password: str) -> bool:
+    set_a = set(list(password))
+    set_b = {"1", "2", "3", "4", "5", "6", "7", "8", "9"}
+    print(set_a)
+    print(set_b)
+    if set_b & set_a and len(set_a)>6 and str(set_a - set_b) != "set()":
+        print(str(set_a - set_b))
+        return True
+    else:
+        return False
+
+
+if __name__ == '__main__':
+
+    # These "asserts" are used for self-checking and not for an auto-testing
+    assert is_acceptable_password('short') == False
+    assert is_acceptable_password('muchlonger') == False
+    assert is_acceptable_password('ashort') == False
+    assert is_acceptable_password('muchlonger5') == True
+    assert is_acceptable_password('sh5') == False
+    assert is_acceptable_password('1234567') == False
+    print("Coding complete? Click 'Check' to earn cool rewards!")
+
+
+def is_acceptable_password(password: str) -> bool:
+    # C1 : the length should be bigger than 6;
+    # C2 : should contain at least one digit, but cannot consist of just digits.
+    # C3 : having numbers or containing just numbers does not apply to the password longer than 9.
+    # C4 : a string should not contain the word "password" in any case.
+    c1 = len(password) > 6
+    c2 = any(map(str.isdigit, password)) and not password.isdigit()
+    c3 = len(password) > 9
+    c4 = 'password' not in password.lower()
+    return c1 and (c2 or c3) and c4
+
+
+if __name__ == '__main__':
+    print("Example:")
+    print(is_acceptable_password('short'))
+
+    # These "asserts" are used for self-checking and not for an auto-testing
+    assert is_acceptable_password('short') == False
+    assert is_acceptable_password('short54') == True
+    assert is_acceptable_password('muchlonger') == True
+    assert is_acceptable_password('ashort') == False
+    assert is_acceptable_password('muchlonger5') == True
+    assert is_acceptable_password('sh5') == False
+    assert is_acceptable_password('1234567') == False
+    assert is_acceptable_password('12345678910') == True
+    print("Coding complete? Click 'Check' to earn cool rewards!")
